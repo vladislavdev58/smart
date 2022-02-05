536f70f7351517807f35b1709bb7f4c667e75b3e
vladislavdev58 <vladislav.dev58@gmail.com>
2022-02-05 11:12:56 +0300

    save data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom"
 import {AppRoutes} from './AppRoutes'
 
+// Сохранил json, если отвалится api, чтобы было (src/assets/data/characters.json)
+
 export const App = () => {
   return (
       <BrowserRouter>
